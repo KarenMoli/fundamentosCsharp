34b3cff135c2314d2be907fa62130d79ac360f97
KarenMoli <124634195+KarenMoli@users.noreply.github.com>
2023-02-08 19:35:07 -0500

    INICIANDO
    Aprendiendo

---
+++ Modulo 1/Modulo 1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Modulo_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Write("1");
+            Console.WriteLine("2");
+        }
+    }
+}
